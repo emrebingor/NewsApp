d7009477a816d7def161c47fa77a6e7ffe7eea8c
emreebingor <emrebingor@unplugdining.com>
2025-04-07 16:23:49 +0300

    Initial Commit

---
+++ ProfileAppTests/ProfileAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppTests.swift
+//  ProfileAppTests
+//
+//  Created by Emre Bingor on 4/7/25.
+//
+
+import Testing
+@testable import ProfileApp
+
+struct ProfileAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
